405830ff5848a56e5a29291dc1c65da69d75bfeb
KrsXishere <lxghozt28@gmail.com>
2025-01-17 21:54:30 +0700

    fix: fixing relation between student and attendance

--- src/app/Filament/Resources/AttendanceResource.php
+++ src/app/Filament/Resources/AttendanceResource.php
@@ -35,19 +35,46 @@ class AttendanceResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('student.nim')
+                    ->searchable()
+                    ->label("Nomor Induk Mahasiswa"),
+                Tables\Columns\TextColumn::make('student.name')
+                    ->searchable()
+                    ->label("Nama Lengkap"),
+                Tables\Columns\TextColumn::make('student.major')
+                    ->searchable()
+                    ->label("Jurusan"),
+                Tables\Columns\TextColumn::make('student.study_program')
+                    ->searchable()
+                    ->label("Program Studi"),
+                Tables\Columns\TextColumn::make('student.year')
+                    ->searchable()
+                    ->label("Angkatan"),
+                Tables\Columns\TextColumn::make('student.email')
+                    ->searchable()
+                    ->label("Alamat Email"),
+                Tables\Columns\TextColumn::make('workshop.title')
+                    ->searchable()
+                    ->label("Judul Workshop"),
+                Tables\Columns\TextColumn::make('check_in_time')
+                    ->searchable()
+                    ->label("Presensi Awal"),
+                Tables\Columns\TextColumn::make('check_out_time')
+                    ->searchable()
+                    ->label("Presensi Akhir"),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
+            ])
+            ->recordUrl(null);
     }
 
     public static function getRelations(): array
@@ -64,5 +91,10 @@ class AttendanceResource extends Resource
             'create' => Pages\CreateAttendance::route('/create'),
             'edit' => Pages\EditAttendance::route('/{record}/edit'),
         ];
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
     }
 }
--- src/app/Models/Attendance.php
+++ src/app/Models/Attendance.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attendance extends Model
 {
@@ -18,15 +19,19 @@ class Attendance extends Model
 	protected $fillable = [
 		'student', 'workshop_id', 'check_in_time', 'check_out_time'
 	];
+
+	protected $casts = [
+		'student' => 'string', // Pastikan ini di-set ke 'string'
+	];
 	
 	// Relasi: Attendance terkait dengan satu mahasiswa (student)
-	public function student()
+	public function student() : BelongsTo
 	{
 		return $this->belongsTo(Student::class, 'student', 'nim');
 	}
 	
 	// Relasi: Attendance terkait dengan satu workshop
-	public function workshop()
+	public function workshop() : BelongsTo
 	{
 		return $this->belongsTo(Workshop::class, 'workshop_id');
 	}
